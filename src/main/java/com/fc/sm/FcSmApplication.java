46c42a6356e93a4a44540ac2c63fb745accf6695
Booki Park <bkbkgg@gmail.com>
2023-06-15 17:32:08 +0900

    initial commit

---
+++ src/main/java/com/fc/sm/FcSmApplication.java
@@ -0,0 +1,13 @@
+package com.fc.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcSmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/sm/FcSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcSmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
